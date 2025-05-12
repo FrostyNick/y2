36c5984893c308dfc2188257df323ef92c213554
FrostyNick <57016218+FrostyNick@users.noreply.github.com>
2025-05-12 00:07:12 -0500

    Init

---
+++ README.md
@@ -0,0 +1,118 @@
+# y2
+
+[![Bash](https://img.shields.io/badge/Bash-3E7720?logo=gnubash&logoColor=FFF)](#)
+[![Code Size](https://custom-icon-badges.demolab.com/github/languages/code-size/FrostyNick/y2?logo=file-code&logoColor=white)](#)
+[![License](https://custom-icon-badges.herokuapp.com/github/license/FrostyNick/y2?logo=law&color=EA3424&labelColor=191724)](LICENSE)
+
+Automate `yt-dlp` downloading, storing, checking for duplicates and watching videos using `mpv`, `vlc`, or an alternative with concise commands.
+
+## Table of Contents
+
+- [Dependencies](#dependencies)
+- [Installation](#installation)
+- [Usage](#usage)
+  - [Examples](#examples)
+  - [Options](#options)
+- [Configuration](#configuration)
+- [Contributing](#contributing)
+- [License](#license)
+
+## Dependencies
+
+You will need the following dependencies installed on your system:
+
+- `bash`
+- `coreutils` - If you don't know what this means, you probably have it.
+- [`yt-dlp`](https://github.com/yt-dlp/yt-dlp) (default) or [`youtube-dl`](https://github.com/youtube-dl/youtube-dl) (for downloading videos)
+- [`mpv`](https://mpv.io/) (default), `cvlc`, [`vlc`](https://www.videolan.org/vlc/) or alternative for viewing videos (for viewing videos)
+- [`fzf`](https://github.com/junegunn/fzf) (to be optional in the future; for interactive selection)
+
+## Installation
+
+1. Clone the repository:
+  ```sh
+  cd ~/projects # wherever you choose to store `y2` git repo, but `ln` as seen later needs to be used again if `y2` is moved.
+  git clone https://github.com/FrostyNick/y2.git
+  ```
+
+2. Make the script executable:
+  ```sh
+  chmod +x ./y2/y2.sh
+  ```
+
+3. Add it to path so it can be run anywhere.
+  
+  ```sh
+  # Check that /usr/local/bin is in $PATH otherwise below will not work (To see where path: echo $PATH)
+  sudo ln -s $HOME/projects/y2.sh /usr/local/bin/y2
+  # For example, if choosing ~/.local/bin instead if above is not found in $PATH:
+  sudo ln -s $HOME/projects/y2.sh $HOME/.local/bin/y2
+  ```
+
+4. Check that it runs.
+
+  `y2 --help`
+
+## Usage
+
+See `y2 --help` for the latest help.
+
+```sh
+y2 [options] <media_url>
+```
+
+### Examples
+
+1. Download a video and save it in `filestore`.
+  ```sh
+  y2 https://youtu.be/pVI_smLgTY0
+  ```
+
+2. Download a video like above with recommended added options (might only work with `yt-dlp`):
+  ```sh
+  y2 0 https://youtu.be/pVI_smLgTY0
+  ```
+
+  The `0` above at the time of writing is replaced with:
+  `-f bestvideo[height<=1080]+bestaudio/best[height<=1080] --embed-chapters --sponsorblock-mark all --embed-metadata --embed-thumbnail --add-metadata --embed-subs --sub-lang en`
+
+3. Download a video with height no bigger than 500px (this is comparable to quality settings in YouTube video); adding some flags from `yt-dlp` as well:
+  ```sh
+  y2 500 --dateafter today-2weeks https://www.youtube.com/watch?v=xsDnEj2Hx4Q
+  ```
+
+4. Get video. This will work if it's in the filename.
+  ```sh
+  y2 -g pVI_smLgTY0
+  ```
+
+### Options
+
+- `-h` or `--help` or empty: Get help.
+- `-g`: Get downloaded video(s) using `grep` and `fzf`. Tested to work on YouTube videos.
+- `0`: Apply extra flags to the filefetch (`yt-dlp`; might work with `youtube-dl`).
+- `<int> != 0`: Set a maximum video height size.
+- `--max <int>`: Same as above.
+
+Every other flag that is not recognized will be passed to `filefetch`.
+
+## Configuration
+
+You can customize the following settings in the start of the script:
+
+- `filefetch`: Set to `yt-dlp` (default) or `youtube-dl`.
+- `fileview`: Set to `mpv` (default) or other media players.
+- `filestore`: Default storage location for downloaded videos (`$HOME/y2`).
+- `filecache`: Default cache location (`$HOME/.cache/y2`).
+
+> [!WARNING]  
+> This may be moved to `$HOME/.config` in the future.
+
+## Contributing
+
+Contributions are welcome! Feel free to open an issue or submit a pull request.
+
+## License
+
+This project is licensed under the UNLICENSE License. See the [UNLICENSE](UNLICENSE) file for details.
+
---
+++ y2.sh
@@ -0,0 +1,150 @@
+#!/usr/bin/env bash
+
+## settings
+
+filefetch="yt-dlp"
+fileview="mpv"
+
+# WARNING: If you update below values, you will need to copy files to the new location for all the videos to be in the same place.
+filestore="$HOME/y2"
+filecache="$HOME/.cache/y2"
+
+## end of settings
+
+if [ "$1" = "--cwd" ]; then
+  echy "WARNING: --cwd is experimental and not stable. Enter to continue."
+  read; filestore="$(pwd)"
+  shift
+else
+  [ -d "$filestore" ] || mkdir -p "$filestore"
+fi
+
+[ -d "$filecache" ] || mkdir -p "$filecache"
+cd "$filecache"
+
+args=("")
+basename=""
+msg=""
+
+echy() {
+  echo "[y2]: $@"
+}
+
+exyt() {
+  echy "Exiting due to error."
+  # "return" after this fn is needed
+}
+
+view() {
+  echy "running: $fileview \"$basename\" from $(pwd)"
+  $fileview "$basename"
+  env ls -lhFG1 --color=auto "$basename"
+  read -p "[y2]: Keep this file? This change is permanent. 3=convert to mp3 and remove original (requires ffmpeg). y/3/n: " msg
+  msg=$(echo "$msg" | tr '[:upper:]' '[:lower:]')
+  if [ "$msg" = "n" ] || [ "$msg" = "no" ]; then
+    rm "$basename" && echy "Removed."
+  else
+    echy "Kept video."
+    if [ "$msg" = "3" ]; then # it would be better to try to use original audio from filefetch. this is lazy solution.
+      echy "Converting to mp3 in new terminal."
+
+      ffmpeg -loglevel warning -v error -stats -i "$basename" "$basename".mp3 || echy Failed to convert. && rm "$basename"
+      # ($TERMINAL -e ffmpeg -loglevel warning -v error -stats -i "$basename" "$basename".mp3) || (echo "backup bc sht code"; ffmpeg -loglevel warning -v error -stats -i "$basename" "$basename".mp3) || echy Failed to convert. && rm "$basename"
+    fi
+  fi
+}
+
+cDup() { # check dup
+  # id=$(echo $1 | sed 's/.*\=//' | sed 's/.*\///' | sed 's/\?.*//') 
+  basename="$(env ls -1 "$filestore" | grep "$(echo "$1" | sed 's/.*\=//' | sed 's/.*\///' | sed 's/\?.*//')")"
+  if [ "$2" = "-x" ] || [ "$2" = "--extract-audio" ]; then
+    basename=$(echo "$basename" | grep 'opus\|mp3' 2> /dev/null || echo "$basename") # prioritize audio when using common audio flags
+  fi
+  basename=$(echo "$basename" | tail -1)
+
+  # echy $basename
+  if [ ! -z "$basename" ]; then
+    read -p "[y2]: Found duplicate '$basename'. Play? (y/n) Otherwise download again." msg
+    msg=$(echo "$msg" | tr '[:upper:]' '[:lower:]')
+    if [ "$msg" = "" ]; then
+      msg="yNull"
+    fi
+  fi
+}
+
+main() {
+  if [ "${#args[@]}" = "0" ]; then
+    cmd="$filefetch $@"
+  else
+    echy "${args[@]}"
+    cmd="$filefetch ${args[@]} $@"
+  fi
+  # below sets basename + msg variables
+  cDup "${@: -1}" "$1"
+  if ! [ "$msg" = "" ]; then # empty = no dup file found
+    if ! [ "$msg" = "n" -o "$msg" = "no" ]; then
+      echy "Skipped download."
+      cd "$filestore";view;return
+    fi
+  fi
+  echy "running: $cmd"
+  # return # dryrun
+  $cmd
+  basename="$(basename *)"
+  if [ "$basename" = "*" ]; then
+    echy "File not found. Possible reasons: Due to $filefetch error / invalid syntax. Not updated $filefetch. Bug with y2; consider making an issue if you've tried everything else."
+    exyt;return
+  fi
+  mv "$basename" "$filestore" && cd "$filestore" && echy "Saved in: "$(pwd)"" && view
+}
+
+if [ $# -eq 0 ] || [ "$1" = "--help" ]; then
+  echo "Automate downloading, storing, checking for duplicates (not related to archive.txt if you know about that), getting and watching videos (mpv [recommended], cvlc, vlc, and some other players work) with a single command."
+  echo
+  # echo "Use this as first argument:"
+  # Below is experimental and may break sometimes.
+  # echo -e "  --cwd      \t Use current working directory instead of $filestore.\n"
+  echo "Choose one of the following options:"
+  echo -e "  -g         \t Get downloaded video(s) with grep and fzf (this doesn't check partial videos in cache). Works with YT videos."
+  echo -e "  0          \t Apply extra flags to $filefetch (made for yt-dlp), which you may or may not like."
+  echo -e "  <int> != 0 \t Set a max video height size"
+  echo -e "  --max <int>\t Same as above"
+  echo
+  echo "Every other flag after will be passed to $filefetch."
+  echo -e "Example:   \t y2 0 https://youtu.be/pVI_smLgTY0"
+  echo -e "Example #2:\t y2 -x https://www.youtube.com/watch?v=xsDnEj2Hx4Q"
+  echo
+  echo "Above downloads audio of the youtube link when filefetch value is set to yt-dlp / youtube-dl."
+  echo "For $filefetch help, use the $filefetch command instead (probably with --help)."
+  echo
+  echo "filefetch is set to $filefetch (default: yt-dlp)"
+  echo "fileview  is set to $fileview (default: mpv)"
+  echo "filestore is set to $filestore (default: \$HOME/y2)"
+  echo "filecache is set to $filecache (default: \$HOME/.cache/y2)"
+  echo
+
+elif [ "$1" = "0" ]; then
+  echy "0 adds these flags to $filefetch:"
+  args=("-f" "bestvideo[height<=1080]+bestaudio/best[height<=1080]" "--embed-chapters" "--sponsorblock-mark" "all" "--embed-metadata" "--embed-thumbnail" "--add-metadata" "--embed-subs" "--sub-lang" "en")
+  # args=("-f" "bestvideo[height<=1080]+bestaudio/best[height<=1080]" "--embed-chapters" "--sponsorblock-mark" "all" "--embed-metadata" "--embed-thumbnail" "--add-metadata" "--embed-subs" "--sub-lang" "en" "--sponsorblock-remove" "sponsor")
+  # Above remove sponsored parts of video.
+  # "--sponsorblock-remove" "all,-intro,-outro,-selfpromo,-preview,-interaction,-poi_highlight" # might not work
+
+
+  shift
+  main "$@"
+elif [[ $1 =~ ^[0-9]+$ ]]; then
+  echy "an integer argument (excluding 0) adds these flags to $filefetch: "
+  args=("-f" "bestvideo[height<=$1]+bestaudio/best[height<=$1]")
+  shift
+  main "$@"
+elif [ "$1" = "-g" ]; then
+  basename=$(env ls -1 "$filestore" | grep -i $(echo $2 | sed 's/.*\=//' | sed 's/.*\///' | sed 's/\?.*//') | fzf -1) && (cd "$filestore";view) || echy "No result found."
+elif [ "$1" = "--max" ]; then
+  echy "--max adds these flags to $filefetch: "
+  args=("-f" "bestvideo[height<=$2]+bestaudio/best[height<=$2]")
+  shift;shift
+  main "$@"
+else
+  main "$@"
+fi
