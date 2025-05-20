8552156c7ddd66ea48af30a632f5cd9146a917ba
FrostyNick <57016218+FrostyNick@users.noreply.github.com>
2025-05-20 15:37:40 -0500

    FIX: Cache files no longer break script + other improvements.
    - FIX: Additonal subtitle and thumbnail files are now handled correctly by script.
    - FIX: youtube-dl (yt-dlp not affected) works now with `y2 0`.
    - -g flag will now ignore subtitle files.
    - Clean-up code.

--- y2.sh
+++ y2.sh
@@ -23,7 +23,7 @@ fi
 cd "$filecache"
 
 args=("")
-basename=""
+bname=""
 msg=""
 
 echy() {
@@ -36,19 +36,19 @@ exyt() {
 }
 
 view() {
-  echy "running: $fileview \"$basename\" from $(pwd)"
-  $fileview "$basename"
-  env ls -lhFG1 --color=auto "$basename"
+  echy "running: $fileview \"$bname\" from $(pwd)"
+  $fileview "$bname"
+  env ls -lhFG1 --color=auto "$bname"
   read -p "[y2]: Keep this file? This change is permanent. 3=convert to mp3 and remove original (requires ffmpeg). y/3/n: " msg
   msg=$(echo "$msg" | tr '[:upper:]' '[:lower:]')
   if [ "$msg" = "n" ] || [ "$msg" = "no" ]; then
-    rm "$basename" && echy "Removed."
+    rm "$bname" && echy "Removed."
   else
     echy "Kept video."
     if [ "$msg" = "3" ]; then # it would be better to try to use original audio from filefetch. this is lazy solution.
       echy "Converting to mp3 in new terminal."
 
-      ffmpeg -loglevel warning -v error -stats -i "$basename" "$basename".mp3 || echy Failed to convert. && rm "$basename"
+      ffmpeg -loglevel warning -v error -stats -i "$bname" "$bname".mp3 || echy Failed to convert. && rm "$bname"
       # ($TERMINAL -e ffmpeg -loglevel warning -v error -stats -i "$basename" "$basename".mp3) || (echo "backup bc sht code"; ffmpeg -loglevel warning -v error -stats -i "$basename" "$basename".mp3) || echy Failed to convert. && rm "$basename"
     fi
   fi
@@ -56,15 +56,15 @@ view() {
 
 cDup() { # check dup
   # id=$(echo $1 | sed 's/.*\=//' | sed 's/.*\///' | sed 's/\?.*//') 
-  basename="$(env ls -1 "$filestore" | grep "$(echo "$1" | sed 's/.*\=//' | sed 's/.*\///' | sed 's/\?.*//')")"
+  bname="$(env ls -1 "$filestore" | grep "$(echo "$1" | sed 's/.*\=//' | sed 's/.*\///' | sed 's/\?.*//')")"
   if [ "$2" = "-x" ] || [ "$2" = "--extract-audio" ]; then
-    basename=$(echo "$basename" | grep 'opus\|mp3' 2> /dev/null || echo "$basename") # prioritize audio when using common audio flags
+    bname=$(echo "$bname" | grep 'opus\|mp3' 2> /dev/null || echo "$bname") # prioritize audio when using common audio flags
   fi
-  basename=$(echo "$basename" | tail -1)
+  bname=$(echo "$bname" | tail -1)
 
   # echy $basename
-  if [ ! -z "$basename" ]; then
-    read -p "[y2]: Found duplicate '$basename'. Play? (y/n) Otherwise download again." msg
+  if [ ! -z "$bname" ]; then
+    read -p "[y2]: Found duplicate '$bname'. Play? (y/n) Otherwise download again." msg
     msg=$(echo "$msg" | tr '[:upper:]' '[:lower:]')
     if [ "$msg" = "" ]; then
       msg="yNull"
@@ -90,12 +90,14 @@ main() {
   echy "running: $cmd"
   # return # dryrun
   $cmd
-  basename="$(basename *)"
-  if [ "$basename" = "*" ]; then
-    echy "File not found. Possible reasons: Due to $filefetch error / invalid syntax. Not updated $filefetch. Bug with y2; consider making an issue if you've tried everything else."
+  bname="$(basename "$(env ls -1t | grep -v ".vtt$\|.srt$\|.lrc$\|.ass$\|.jpg$\|.jpeg" | head -1)")"
+  if [ "$bname" = "*" ]; then
+    echy "File $bname not found. Possible reasons: Due to $filefetch error / invalid syntax. Not updated $filefetch. Bug with y2; consider making an issue if you've tried everything else."
     exyt;return
   fi
-  mv "$basename" "$filestore" && cd "$filestore" && echy "Saved in: "$(pwd)"" && view
+  mv --verbose *.{ass,lrc,srt,vtt} "$filestore" 2> /dev/null && echy "Found and moved downloaded subtitle files."
+  mv --verbose *.{jpg,jpeg} "$filestore" 2> /dev/null && echy "Found and moved downloaded images. (Likely thumbnail of video)."
+  (mv "$bname" "$filestore" && cd "$filestore" && echy "Saved in: "$(pwd)"" && view) || exyt;return
 }
 
 if [ $# -eq 0 ] || [ "$1" = "--help" ]; then
@@ -105,9 +107,9 @@ if [ $# -eq 0 ] || [ "$1" = "--help" ]; then
   # echo -e "  --cwd      \t Broken: Use current working directory instead of $filestore.\n"
   echo "Choose one of the following options:"
   echo -e "  -g         \t Get downloaded video(s) with grep and fzf (this doesn't check partial videos in cache). Works with YT videos."
-  echo -e "  0 <int?>   \t Apply useful extra flags to $filefetch (made for yt-dlp). Optional 2nd argument: Max video height size."
-  echo -e "  <int> != 0 \t Max video height size"
-  echo -e "  --max <int>\t Same as above"
+  echo -e "  0 <int?>   \t Apply useful extra flags to yt-dlp / youtube-dl. Optional 2nd argument: Max video height."
+  echo -e "  <int> != 0 \t Max video height."
+  echo -e "  --max <int>\t Same as above."
   echo
   echo "Every other flag after will be passed to $filefetch."
   echo -e "Example:   \t y2 0 480 https://youtu.be/pVI_smLgTY0"
@@ -125,11 +127,20 @@ if [ $# -eq 0 ] || [ "$1" = "--help" ]; then
 elif [ "$1" = "0" ]; then
   if [[ $2 =~ ^[0-9]+$ ]]; then
     echy "0 $2 adds these flags to $filefetch:"
-    args=("-f" "bestvideo[height<=$2]+bestaudio/best[height<=$2]" "--embed-chapters" "--sponsorblock-mark" "all" "--embed-metadata" "--embed-thumbnail" "--add-metadata" "--embed-subs" "--sub-lang" "en")
+    argmax="bestvideo[height<=$2]+bestaudio/best[height<=$2]"
     shift
   else
+    argmax="bestvideo[height<=1080]+bestaudio/best[height<=1080]"
     echy "0 adds these flags to $filefetch:"
-    args=("-f" "bestvideo[height<=1080]+bestaudio/best[height<=1080]" "--embed-chapters" "--sponsorblock-mark" "all" "--embed-metadata" "--embed-thumbnail" "--add-metadata" "--embed-subs" "--sub-lang" "en")
+  fi
+  if [ "$filefetch" = "youtube-dl" ]; then
+    args=("-f" "$argmax" "--add-metadata" "--sub-lang" "en" "--write-sub" "--write-auto-sub" "--embed-subs") # "--embed-thumbnail" saves as a separate file usually, so not included by default.
+  else
+    args=("-f" "$argmax" "--embed-chapters" "--sponsorblock-mark" "all" "--embed-metadata" "--embed-thumbnail" "--add-metadata" "--sub-langs" "en" "--write-subs" "--write-auto-subs" "--embed-subs" "--compat-options no-keep-subs")
+    # --compat-options is needed for yt-dlp to embed both auto and regular subtitles at the moment: https://github.com/yt-dlp/yt-dlp/issues/630#issuecomment-893659460
+    if ! [ "$filefetch" = "yt-dlp" ]; then # yt-dlp forks probably exist
+      echy "WARNING: yt-dlp is being used as fallback (filefetch [default: yt-dlp] = $filefetch). If this is wrong, next time remove the 0, change filefetch or create an issue."
+    fi
   fi
   # args=("-f" "bestvideo[height<=1080]+bestaudio/best[height<=1080]" "--embed-chapters" "--sponsorblock-mark" "all" "--embed-metadata" "--embed-thumbnail" "--add-metadata" "--embed-subs" "--sub-lang" "en" "--sponsorblock-remove" "sponsor")
   # Above remove sponsored parts of video.
@@ -143,7 +154,7 @@ elif [[ $1 =~ ^[0-9]+$ ]]; then
   shift
   main "$@"
 elif [ "$1" = "-g" ]; then
-  basename=$(env ls -1 "$filestore" | grep -i $(echo $2 | sed 's/.*\=//' | sed 's/.*\///' | sed 's/\?.*//') | fzf -1) && (cd "$filestore";view) || echy "No result found."
+  bname=$(env ls -1 "$filestore" | grep -i $(echo $2 | sed 's/.*\=//' | sed 's/.*\///' | sed 's/\?.*//') | grep -v ".vtt$\|.srt$\|.lrc$\|.ass$\|" | fzf -1) && (cd "$filestore";view) || echy "No result found."
 elif [ "$1" = "--max" ]; then
   echy "--max adds these flags to $filefetch: "
   args=("-f" "bestvideo[height<=$2]+bestaudio/best[height<=$2]")
